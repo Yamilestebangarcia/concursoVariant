48e80b8ea730ddb117ee7c285caf8f22fb848956
Yamilestebangarcia <yamil_mont@hotmail.com>
2020-10-05 22:46:02 +0200

    concurso pero una variante con comentarios

--- src/concursovariante/ConcursoVariante.java
+++ src/concursovariante/ConcursoVariante.java
@@ -8,44 +8,42 @@ public class ConcursoVariante {
 
    
     public static void main(String[] args) {
-         
+         //variables entrada
         String nombre; 
         String apellido1;
         String apellido2;
-        int x=2;
-        int y=3;
-        String resultado;
-        Scanner teclado = new Scanner( System.in );
-        System.out.println("introduce tu nombre");
-        nombre=teclado.next();
-         nombre =nombre.toUpperCase().charAt(0) + nombre.substring(1, nombre.length()).toLowerCase();
+       //entrada de datos
+        Scanner teclado = new Scanner( System.in );//instancio la calse scanner
+        System.out.println("introduce tu nombre");// imprimo en pantalla
+        nombre=teclado.next();//recojo los datos
+         nombre =nombre.toUpperCase().charAt(0) + nombre.substring(1, nombre.length()).toLowerCase();// primer obtengo la primera palaba en mayuscula y luego la primera letra de la palabra y la junto
          
-        System.out.println("introduce tu primer apellido");
-         apellido1 =teclado.next();
-         apellido1 =apellido1.toUpperCase().charAt(0) + apellido1.substring(1,apellido1.length()).toLowerCase();
+        System.out.println("introduce tu primer apellido");// imprimo en pantalla
+         apellido1 =teclado.next();//recojo los datos
+         apellido1 =apellido1.toUpperCase().charAt(0) + apellido1.substring(1,apellido1.length()).toLowerCase();// primer obtengo la primera palaba en mayuscula y luego la primera letra de la palabra y la junto
       
-        System.out.println("introduce tu segundo apellido");
-         apellido2 =teclado.next(); 
-        apellido2 =apellido2.toUpperCase().charAt(0) + apellido2.substring(1,apellido2.length()).toLowerCase();
+        System.out.println("introduce tu segundo apellido"); // imprimo en pantalla
+         apellido2 =teclado.next(); //recojo los dato
+        apellido2 =apellido2.toUpperCase().charAt(0) + apellido2.substring(1,apellido2.length()).toLowerCase();// primer obtengo la primera palaba en mayuscula y luego la primera letra de la palabra y la junto
    
-        boolean esPar1= (apellido1.length()%2)==0;
-         boolean esPar2= (apellido2.length()%2)==0;
+        boolean esPar1= (apellido1.length()%2)==0;//compruebo si es pao o impar
+         boolean esPar2= (apellido2.length()%2)==0;//compruebo si es pao o impar
     
-    String letraUltima=nombre.substring(nombre.length()-1);
+    String letraUltima=nombre.substring(nombre.length()-1); //obtengo la ultima letra. 
    
        
    
-       
+       //compruebo si la ultima letra tiene vocales
            if(letraUltima.equalsIgnoreCase("a") ||letraUltima.equalsIgnoreCase("e") || letraUltima.equalsIgnoreCase("i") || letraUltima.equalsIgnoreCase("o") || letraUltima.equalsIgnoreCase("u")){
-         
+         // compruebo si ambas son pares o impares.
                if(esPar1==esPar2){
-                   System.out.println(nombre+" "+apellido1+" "+apellido2+". Es apta para el concurso");
+                   System.out.println(nombre+" "+apellido1+" "+apellido2+". Es apta para el concurso"); // imprimo en pantalla
                }else{ 
-                   System.out.println(nombre+" "+apellido1+" "+apellido2+". No apta para el concurso");
+                   System.out.println(nombre+" "+apellido1+" "+apellido2+". No apta para el concurso"); // imprimo en pantalla
                }
                
          }else{
-         System.out.println(nombre+" "+apellido1+" "+apellido2+". No apta para el concurso"); 
+         System.out.println(nombre+" "+apellido1+" "+apellido2+". No apta para el concurso");  // imprimo en pantalla
          }
         
     }
